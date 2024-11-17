9aaa007879f5bd3430955b2274af9122e98e7e20
AbhishekMaithani011 <109787856+AbhishekMaithani011@users.noreply.github.com>
2024-11-17 22:44:44 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Thoughts for life</h1>
+    </header>
+  );
+}
+
+export { Header };
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Today's Thought:</h1>
+      <p>
+        Every sunrise brings a new opportunity to shine brighter than before.
+      </p>
+    </div>
+  );
+}
+
+export { Note };
